72e845a692801fa7c6cfabf08c4c0e71ef83ac44
milomccarty99 <milomccarty99@gmail.com>
2024-03-18 21:24:28 -0700

    safe point

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # spinning_lantern_saas
 SaaS for coordinating sales events with automated docker container deployment.
+
+## to-do
+- implement `help` command
+- automate docker container spinup
+- give users tasks like buying and selling stock of a product
+- docker to website
+  - static website for product spun off from a single docker container
+- bright colored histograms to show sales over time.
+- trading walls diagram
+- scripting for `events` that happen i.e. sales 
+  - cancel sales
+  - drop or raise prices
+  - etc
+
+## desired functionality
+- product pane, histogram pane, console pane
+- product database, user database
+- user login at terminal
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+use std::io::{stdin, stdout, Write};
+//use std::cmp;
+//use termion::color;
+
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+    
+        let mut command: String = String::from("");
+        stdin().read_line(&mut command);
+        let mut tokenized_command = command.trim().split(' ');
+        let mut tc = tokenized_command.collect::<Vec<&str>>();
+        //if tokenized_command.count() <= 0 
+        //{
+        //    continue;
+        //}
+        let mut cmd = tc[0];
+        match cmd {
+            "help" => {print_help_msg()},
+            "sale" => {println!("selling")},// {tokenized_command}");},
+            "invoice" => {println!("invoice for {}",tc[1])},
+            "q" => {break;},
+            "quit" => {break;},
+            _ => {println!("command not recognized:")},// {tokenized_command}")},
+        }
+
+
+    }
+}
+
+fn print_help_msg() {
+    println!("commands that you can run:");
+    println!("help - display this help message");
+    println!("sale - start as sale");
+    println!("invoice - invoice someone for a product");
+    println!("quit - quit the application");
+
 }
